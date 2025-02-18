4ae90436fda67c4a35415632f0bd689c0ffcd910
DaniPrr90 <danilo.pirro@libero.it>
2025-02-18 16:35:13 +0100

    add:modiche codice inserimento fragments e MSQL

--- src/main/java/org/lessons/java/spring/lamiapizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring/lamiapizzeria/controller/PizzaController.java
@@ -1,23 +1,42 @@
+package org.lessons.java.spring.lamiapizzeria.controller;
+
 import java.util.List;
 
+import org.lessons.java.spring.lamiapizzeria.model.Pizza;
+import org.lessons.java.spring.lamiapizzeria.repo.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/pizze")
+@RequestMapping("/")
 public class PizzaController {
-
     @Autowired
     private PizzaRepository pizzaRepository;
 
-    @GetMapping
+    @GetMapping("/")
     public String index(Model model) {
         List<Pizza> listapizze = pizzaRepository.findAll();
         model.addAttribute("pizze", listapizze);
-        return "pizze/index";
+        return "/pizze/fragments/index";
+    }
+
+    @GetMapping("/pizza")
+    public String seePizza(@RequestParam(name = "query") String query, Model model) {
+        // Troviamo tutte le pizze che contengono il nome della query
+        // List<Pizza> pizze = pizzaRepository.findByNomeContaining(query);
+        List<Pizza> pizze = pizzaRepository.findByNomeContaining(query);
+
+        if (query.equals("") || query.equals("")) {
+            model.addAttribute("message", "Pizza non trovata");
+            return "/pizze/fragments/pizza";
+        } else {
+            model.addAttribute("pizza", pizze);
+            return "/pizze/fragments/pizza";
+        }
     }
 
 }
--- src/main/java/org/lessons/java/spring/lamiapizzeria/repo/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring/lamiapizzeria/repo/PizzaRepository.java
@@ -1,9 +1,16 @@
 package org.lessons.java.spring.lamiapizzeria.repo;
 
+import java.util.List;
+
+import org.lessons.java.spring.lamiapizzeria.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+    public List<Pizza> findById(int id);
 
+    public List<Pizza> findByNomeContaining(String name);
+
+    public List<Pizza> findByNomeContainingIgnoreCase(String query);
 }
